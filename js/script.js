59196a3f442b4f23f127ad26c61f10c420e78713
jason2020 <jtay20@punahou.edu>
2016-10-14 13:23:26 -1000

    Fixed bugs and loopholes.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var button = document.getElementById("submit");
 var text = document.getElementById("rna");
+
 button.addEventListener("click", function() {
 	var result = rnaChainBreaker(text.value.toUpperCase());
 	this.parentNode.reset();
@@ -7,9 +8,10 @@ button.addEventListener("click", function() {
 });
 
 function rnaChainBreaker(rnaChain) {
-	var array = [];
-	var rnaStrands = rnaChain.match(/.{1,3}/g);
-	var length = rnaStrands.length;
+	if ((rnaChain.length % 3 == 0)) {
+		var array = [];
+		var rnaStrands = rnaChain.match(/.{1,3}/g);
+		var length = rnaStrands.length;
 	
 		for (var i = 0; i < length; i++) {
 			var rnaChain = rnaStrands[i].split("");
@@ -17,6 +19,9 @@ function rnaChainBreaker(rnaChain) {
 			var aminoAcid = getAminoAcid(processedRna);
 			array.push(aminoAcid);
 		}
+	} else {
+		return false;
+	}
 	console.log(array);
 	return array;
 }
@@ -31,7 +36,8 @@ function processRnaStrand (rnaStrand) {
 	} else if (rnaStrand[0] == "G") {
 		rnaStrand.splice(0, 1, 3)
 	} else {
-		return "";
+		console.log("1st letter");
+		return false;
 	}
 	
 	if (rnaStrand[1] == "U") {
@@ -43,80 +49,95 @@ function processRnaStrand (rnaStrand) {
 	} else if (rnaStrand[1] == "G") {
 		rnaStrand.splice(1, 1, 3)
 	} else {
-		return "";
+		console.log("2nd letter");
+		return false;
 	}
 	return rnaStrand;
 }
 
 function getAminoAcid(processRna) {
-	if (processRna == "") {
-		return "";
-	}
-	// array[0] = U, array[1] = C, array[2] = A, array[3] = G
-	// inner array organized by: UCAG
-	var combinations = [[
-							["Phenylalanine", "Leucine"], ["Serine"], ["Tyrosine", "Stop Codon"], ["Cysteine", "Stop Codon", "Typtophan"]
-						],
-						[
-							["Leucine"], ["Proline"], ["Histidine", "Glutamine"], ["Arginine"]
-						],
-						[
-							["Isoleucine", "Methionine"], ["Theronine"], ["Asparagine", "Lysine"], ["Serine", "Arginine"]
-						],
-						[
-							["Valine"], ["Alanine"], ["Aspartic Acid", "Glutamic Acid"], ["Glycine"]
-						]],
-		resultAminoAcid = "",
-		index1 = processRna[0],
-		index2 = processRna[1];
-		
-	if (combinations[index1][index2].length == 1) {
-		resultAminoAcid = combinations[index1][index2][0]
-	} else if (combinations[index1][index2].length == 2) {
-		if ((processRna[2] == "U") || (processRna[2] == "C")) {
-			resultAminoAcid = combinations[index1][index2][0]
-		} else if ((processRna[2] == "A") || (processRna[2] == "G")) {
-			resultAminoAcid = combinations[index1][index2][1]
-		} else {
-			return "";
-		}
-	} else if (combinations[index1][index2].length == 3) {
-		if ((processRna[2] == "U") || (processRna[2] == "C")) {
-			resultAminoAcid = combinations[index1][index2][0]
-		} else if (processRna[2] == "A") {
-			resultAminoAcid = combinations[index1][index2][1]
-		} else if (processRna[2] == "G") {
-			resultAminoAcid = combinations[index1][index2][2]
-		} else {
-			return "";
-		}
+	if (processRna == false) {
+		return false;
 	} else {
-		return "";
+		// array[0] = U, array[1] = C, array[2] = A, array[3] = G
+		// inner array organized by: UCAG
+		var combinations = [[
+								["Phenylalanine", "Leucine"], ["Serine"], ["Tyrosine", "Stop Codon"], ["Cysteine", "Stop Codon", "Typtophan"]
+							],
+							[
+								["Leucine"], ["Proline"], ["Histidine", "Glutamine"], ["Arginine"]
+							],
+							[
+								["Isoleucine", "Methionine"], ["Theronine"], ["Asparagine", "Lysine"], ["Serine", "Arginine"]
+							],
+							[
+								["Valine"], ["Alanine"], ["Aspartic Acid", "Glutamic Acid"], ["Glycine"]
+							]],
+			resultAminoAcid = "",
+			index1 = processRna[0],
+			index2 = processRna[1];
+			
+		if (combinations[index1][index2].length == 1) {
+			resultAminoAcid = combinations[index1][index2][0]
+		} else if (combinations[index1][index2].length == 2) {
+			if ((processRna[2] == "U") || (processRna[2] == "C")) {
+				resultAminoAcid = combinations[index1][index2][0]
+			} else if ((processRna[2] == "A") || (processRna[2] == "G")) {
+				resultAminoAcid = combinations[index1][index2][1]
+			} else {
+				return false;
+			}
+		} else if (combinations[index1][index2].length == 3) {
+			if ((processRna[2] == "U") || (processRna[2] == "C")) {
+				resultAminoAcid = combinations[index1][index2][0]
+			} else if (processRna[2] == "A") {
+				resultAminoAcid = combinations[index1][index2][1]
+			} else if (processRna[2] == "G") {
+				resultAminoAcid = combinations[index1][index2][2]
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+		return resultAminoAcid;
 	}
-	return resultAminoAcid;
 }
 
 function addPicture(results) {
-	var length = results.length;
-	var putResults = document.getElementById("results");
-	putResults.innerHTML = "";
-	for (var i = 0; i < length; i++) {
-		var name = results[i].toLowerCase();
-		var container = document.createElement("div");
-		container.className = "aminoAcid";
-		var text = document.createElement("div");
-		text.innerHTML = results[i];
-		text.className = "aminoAcidName";
-		var picture = document.createElement("img");
-		picture.className = "picture";
-		picture.setAttribute("src", "images/" + name + ".png");
-		container.appendChild(text);
-		container.appendChild(picture);
-		putResults.appendChild(container);
+	console.log(results);
+	if (results == false) {
+		error();
+	} else if (results.includes(false) == true){
+		error();
+	} else {
+		var length = results.length;
+		var putResults = document.getElementById("results");
+		putResults.innerHTML = "";
+		for (var i = 0; i < length; i++) {
+			var name = results[i].toLowerCase();
+			var container = document.createElement("div");
+			container.className = "aminoAcid";
+			var text = document.createElement("div");
+			text.innerHTML = results[i];
+			text.className = "aminoAcidName";
+			var picture = document.createElement("img");
+			picture.className = "picture";
+			picture.setAttribute("src", "images/" + name + ".png");
+			container.appendChild(text);
+			container.appendChild(picture);
+			putResults.appendChild(container);
+		}
 	}
 }
 
 function error() {
-	alert("Error in the RNA sequence :(");
-	return false;
+	swal({
+		title: "Error",
+		text: "Something is wrong with the RNA sequence that you have entered,",
+		type: "error",
+		showConfirmButton: false,
+		allowOutsideClick: true,
+		timer: 1000
+	});
 }
